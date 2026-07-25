162762dbe482ba6a6c6c79e8ddbe4924596e7d53
Rodrigo Martins <rodrigomartins8828@gmail.com>
2026-07-25 19:28:55 +0100

    start working at graph

---
+++ src/graph.h
@@ -0,0 +1,21 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <string>
+
+class Vertex;
+
+class Graph {
+
+private:
+    std::vector<Vertex*> vertexSet;
+
+public:
+    void addVertex(Vertex* v);
+    void addEdge(const std::string& location1,const std::string& location2,int driving,int walking);
+    Vertex* findVertexByLocation(const std::string& location);
+    Vertex* findVertexByID(int id);
+};
+
+#endif
