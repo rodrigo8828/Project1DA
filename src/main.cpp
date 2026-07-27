63a579d2a70c921b9daa69196b681327f45d3da3
Rodrigo Martins <rodrigomartins8828@gmail.com>
2026-07-27 15:41:00 +0100

    parser done

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
-using namespace std;
 
 #include "graph.h"
-#include "edge.h"
-#include "vertex.h"
-#include "graph.h"
 #include "parser.h"
+#include "vertex.h"
+#include "edge.h"
 
 int main() {
 
     Graph graph;
 
-    Parser::readLocations("../For Students-20250213T135713Z-001/For Students/Locations.csv", graph);
+    Parser::readLocations(
+        "../For Students-20250213T135713Z-001/For Students/Locations.csv",
+        graph
+    );
+
+    Parser::readDistances(
+        "../For Students-20250213T135713Z-001/For Students/Distances.csv",
+        graph
+    );
+
+    Vertex* v = graph.findVertexByCode("PA9908");
+
+    if (v == nullptr) {
+        std::cout << "Vertex nao encontrado!" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Local: " << v->getLocation() << std::endl;
+    std::cout << "Code: " << v->getCode() << std::endl;
+    std::cout << "Ligacoes:\n";
+
+    for (Edge* e : v->getCon()) {
+
+        std::cout << " -> "
+                  << e->getDestination()->getCode()
+                  << " | Driving: ";
+
+        if (e->getDriving() == -1) {
+            std::cout << "X";
+        }
+        else {
+            std::cout << e->getDriving();
+        }
+
+        std::cout << " | Walking: "
+                  << e->getWalking()
+                  << std::endl;
+    }
 
     return 0;
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -12,8 +12,12 @@ void Parser::readLocations(const std::string& filename, Graph& graph) {
     std::string line;
     int lineNumber = 0;
     while (std::getline(file, line)) {
+        
         lineNumber++;
         if (lineNumber == 1) {
+            continue;
+        }
+        if (line.empty()) {
             continue;
         }
     
@@ -26,7 +30,7 @@ void Parser::readLocations(const std::string& filename, Graph& graph) {
     getline(ss, id, ',');
     getline(ss, code, ',');
     getline(ss, parking, ',');
-
+    
     int id_ = std::stoi(id);
     bool parking_ = std::stoi(parking);
 
@@ -34,6 +38,44 @@ void Parser::readLocations(const std::string& filename, Graph& graph) {
     graph.addVertex(v);
     
     }
+
 }
+    void Parser::readDistances(const std::string& filename, Graph& graph){
+        std::ifstream file(filename);
+        std::string line;
+        int lineNumber = 0;
+        while (std::getline(file, line)) {
+            lineNumber++;
+            if (lineNumber == 1) {
+                continue;
+            }
+            if (line.empty()) {
+                continue;
+            }
+        
+        std::stringstream ss(line);
+        std::string location1;
+        std::string location2;
+        std::string driving;
+        std::string walking;
+        getline(ss, location1, ',');
+        getline(ss, location2, ',');
+        getline(ss, driving, ',');
+        getline(ss, walking, ',');
+
+        int driving_;
+
+        if (driving == "X") {
+            driving_ = -1;
+        }
+        else {
+            driving_ = std::stoi(driving);
+        }
+
+        int walking_ = std::stoi(walking);
+
+        graph.addEdge(location1, location2, driving_, walking_);
+        }
+    }
 
 
