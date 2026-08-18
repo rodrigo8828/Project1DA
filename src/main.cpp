eb43450863c438dd2a18e9dc3805d047705b9f51
Rodrigo Martins <rodrigomartins8828@gmail.com>
2026-08-18 17:17:55 +0100

    output txt done

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "graph.h"
+#include "vertex.h"
 #include "parser.h"
 
 int main() {
+
+    Graph graph;
+
+    Parser::readLocations(
+        "../For Students-20250213T135713Z-001/For Students/Locations_small.csv",
+        graph
+    );
+
+    Parser::readDistances(
+        "../For Students-20250213T135713Z-001/For Students/Distances_small.csv",
+        graph
+    );
+
     InputData data = Parser::readInput("../input.txt");
 
     std::ofstream output("../output.txt");
@@ -14,10 +29,84 @@ int main() {
         return 0;
     }
 
-    output << "Source:" << data.source << "\n";
-    output << "Destination:" << data.destination << "\n";
+    if (data.includeNode == -1) {
+
+        graph.algorithm(
+            data.source,
+            data.avoidNodes,
+            data.avoidSegments
+        );
+
+        std::vector<int> route =
+            graph.getPath(data.destination);
+
+        if (route.empty()) {
+            output << "Restricted Route: none\n";
+        }
+        else {
+
+            Vertex* destination =
+                graph.findVertexByID(data.destination);
+
+            int totalTime =
+                destination->getBestDistance();
+
+            output << "Restricted Route: [";
+
+            for (std::size_t i = 0; i < route.size(); i++) {
+
+                output << route[i];
+
+                if (i < route.size() - 1) {
+                    output << ", ";
+                }
+            }
+
+            output << "] Time: "
+                   << totalTime
+                   << "\n";
+        }
+    }
+
+    else {
+
+        int totalTime = 0;
+
+        std::vector<int> route =
+            graph.IncludeNode(
+                data.source,
+                data.destination,
+                data.includeNode,
+                totalTime,
+                data.avoidNodes,
+                data.avoidSegments
+            );
+
+        if (route.empty()) {
+            output << "Restricted Route: none\n";
+        }
+        else {
+
+            output << "Restricted Route: [";
+
+            for (std::size_t i = 0; i < route.size(); i++) {
+
+                output << route[i];
+
+                if (i < route.size() - 1) {
+                    output << ", ";
+                }
+            }
+
+            output << "] Time: "
+                   << totalTime
+                   << "\n";
+        }
+    }
 
     output.close();
+
+    std::cout << "output.txt criado com sucesso.\n";
 
     return 0;
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -11,81 +11,117 @@ void Parser::readLocations(const std::string& filename, Graph& graph) {
     std::ifstream file(filename);
     std::string line;
     int lineNumber = 0;
+
     while (std::getline(file, line)) {
-        
+
         lineNumber++;
+
         if (lineNumber == 1) {
             continue;
         }
+
         if (line.empty()) {
             continue;
         }
-    
-    std::stringstream ss(line);
-    std::string location;
-    std::string id;
-    std::string code;
-    std::string parking;
-    getline(ss, location, ',');
-    getline(ss, id, ',');
-    getline(ss, code, ',');
-    getline(ss, parking, ',');
-    
-    int id_ = std::stoi(id);
-    bool parking_ = std::stoi(parking);
 
-    Vertex* v = new Vertex(location, id_, code, parking_);
-    graph.addVertex(v);
-    
+        std::stringstream ss(line);
+
+        std::string location;
+        std::string id;
+        std::string code;
+        std::string parking;
+
+        std::getline(ss, location, ',');
+        std::getline(ss, id, ',');
+        std::getline(ss, code, ',');
+        std::getline(ss, parking, ',');
+
+        int id_ = std::stoi(id);
+        bool parking_ = std::stoi(parking);
+
+        Vertex* v = new Vertex(location, id_, code, parking_);
+
+        graph.addVertex(v);
     }
 
+    file.close();
 }
-void Parser::readDistances(const std::string& filename, Graph& graph){
+
+void Parser::readDistances(const std::string& filename, Graph& graph) {
+
     std::ifstream file(filename);
     std::string line;
     int lineNumber = 0;
+
     while (std::getline(file, line)) {
+
         lineNumber++;
+
         if (lineNumber == 1) {
             continue;
         }
+
         if (line.empty()) {
             continue;
         }
-    
-    std::stringstream ss(line);
-    std::string location1;
-    std::string location2;
-    std::string driving;
-    std::string walking;
-    getline(ss, location1, ',');
-    getline(ss, location2, ',');
-    getline(ss, driving, ',');
-    getline(ss, walking, ',');
 
-    int driving_;
+        std::stringstream ss(line);
 
-    if (driving == "X") {
-        driving_ = -1;
+        std::string location1;
+        std::string location2;
+        std::string driving;
+        std::string walking;
+
+        std::getline(ss, location1, ',');
+        std::getline(ss, location2, ',');
+        std::getline(ss, driving, ',');
+        std::getline(ss, walking, ',');
+
+        int driving_;
+
+        if (driving == "X") {
+            driving_ = -1;
+        }
+        else {
+            driving_ = std::stoi(driving);
+        }
+
+        int walking_ = std::stoi(walking);
+
+        graph.addEdge(
+            location1,
+            location2,
+            driving_,
+            walking_
+        );
     }
-    else {
-        driving_ = std::stoi(driving);
-    }
 
-    int walking_ = std::stoi(walking);
-
-    graph.addEdge(location1, location2, driving_, walking_);
-    }
+    file.close();
 }
 
 InputData Parser::readInput(const std::string& filename) {
+
     InputData data;
 
     std::ifstream file(filename);
 
+    if (!file.is_open()) {
+        std::cout << "Erro ao abrir o ficheiro de input.\n";
+        return data;
+    }
+
     std::string line;
 
     while (std::getline(file, line)) {
+
+        if (!line.empty() && line.back() == '\r') {
+            line.pop_back();
+        }
+
+        if (line.empty()) {
+            continue;
+        }
+
         std::size_t separator = line.find(':');
 
         if (separator == std::string::npos) {
@@ -99,45 +135,71 @@ InputData Parser::readInput(const std::string& filename) {
             data.mode = value;
         }
 
-        if (key == "Source") {
-            data.source = std::stoi(value);
-        }
-        if (key == "Destination") {
-            data.destination = std::stoi(value);
-        }
-
-        if (key == "IncludeNode") {
+        else if (key == "Source") {
             if (!value.empty()) {
-            data.includeNode = std::stoi(value);
-        }
-        }
-        if (key == "AvoidNodes") {
-            std::stringstream ss(value);
-            std::string node;
-
-        while (std::getline(ss, node, ',')) {
-            if (!node.empty()) {
-            data.avoidNodes.push_back(std::stoi(node));
+                data.source = std::stoi(value);
             }
         }
-        }
-        if (key == "AvoidSegments") {
-        std::stringstream ss(value);
-        char ch;
-        int a, b;
 
-        while (ss >> ch >> a >> ch >> b >> ch) {
-        data.avoidSegments.push_back({a, b});
+        else if (key == "Destination") {
+            if (!value.empty()) {
+                data.destination = std::stoi(value);
+            }
+        }
 
-        if (ss.peek() == ',') {
-            ss.ignore();
+        else if (key == "IncludeNode") {
+            if (!value.empty()) {
+                data.includeNode = std::stoi(value);
+            }
         }
+
+        else if (key == "AvoidNodes") {
+
+            if (!value.empty()) {
+
+                std::stringstream ss(value);
+                std::string node;
+
+                while (std::getline(ss, node, ',')) {
+
+                    if (!node.empty()) {
+                        data.avoidNodes.push_back(
+                            std::stoi(node)
+                        );
+                    }
+                }
+            }
         }
+
+        else if (key == "AvoidSegments") {
+
+            if (!value.empty()) {
+
+                std::stringstream ss(value);
+
+                char ch;
+                int a;
+                int b;
+
+                while (
+                    ss >> ch
+                       >> a
+                       >> ch
+                       >> b
+                       >> ch
+                ) {
+
+                    data.avoidSegments.push_back({a, b});
+
+                    if (ss.peek() == ',') {
+                        ss.ignore();
+                    }
+                }
+            }
         }
     }
+
     file.close();
 
     return data;
 }
-
-
